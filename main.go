40ffe95c252ce2c1839ae24e40c0c42e4bebad05
Darlan <darlaneduardoguimaraes@gmail.com>
2024-01-30 20:13:49 -0300

    Slice information Commits

--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
 	"io"
+	"log"
 	"os"
+	"time"
 )
+
+type Commmit struct {
+	Key   time.Time
+	Count int
+}
 
 func verifierDir(folder string) bool {
 	f, err := os.Open(folder)
@@ -31,9 +40,63 @@ func verifierDir(folder string) bool {
 	return false
 }
 
+func MapCommits(folder string) []Commmit {
+	repo, err := git.PlainOpen(folder)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ref, err := repo.Head()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cIter, err := repo.Log(&git.LogOptions{From: ref.Hash()})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	SliceWeek := []Commmit{}
+	var aux time.Time
+	var auxTwo time.Time
+	err = cIter.ForEach(func(commit *object.Commit) error {
+		if len(SliceWeek) == 0 {
+			aux = commit.Author.When
+			SliceWeek = append(SliceWeek, Commmit{commit.Author.When, 1})
+		} else {
+			if auxTwo.Format("01/02/2006") != commit.Author.When.Format("01/02/2006") {
+				SliceWeek = append(SliceWeek, Commmit{commit.Author.When, 1})
+				aux = commit.Author.When
+			} else {
+				for i := range SliceWeek {
+					if SliceWeek[i].Key == aux {
+						SliceWeek[i].Count += 1
+						break
+					}
+				}
+			}
+		}
+		auxTwo = commit.Author.When
+		return nil
+	})
+
+	return SliceWeek
+}
+
 func main() {
 	var folder string
 	flag.StringVar(&folder, "add", "", "novo diretório para análise")
 	flag.Parse()
-	fmt.Println(verifierDir(folder))
+	if verifierDir(folder) {
+		MapCommits(folder)
+		SliceCommit := MapCommits(folder)
+
+		for i := range SliceCommit {
+			fmt.Printf("%q ----- %d\n", SliceCommit[i].Key, SliceCommit[i].Count)
+		}
+
+	}
 }
