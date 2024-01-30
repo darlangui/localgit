eb24f795b40a5e408a398a52fbbe1a36f5c627c0
Darlan <darlaneduardoguimaraes@gmail.com>
2024-01-30 11:19:37 -0300

    verifica se tem .git no diretório

--- main.go
+++ main.go
@@ -1,10 +1,39 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
+
+func verifierDir(folder string) bool {
+	f, err := os.Open(folder)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		files, err := f.ReadDir(1)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			continue
+		}
+
+		if files[0].Name() == ".git" {
+			return true
+		}
+	}
+
+	return false
+}
 
 func main() {
 	var folder string
 	flag.StringVar(&folder, "add", "", "novo diretório para análise")
 	flag.Parse()
-	print(folder)
+	fmt.Println(verifierDir(folder))
 }
