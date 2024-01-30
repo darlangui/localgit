3305caa4c661f0d9385ad03fce8ab584c5cff526
Darlan <darlaneduardoguimaraes@gmail.com>
2024-01-30 09:23:04 -0300

    flag

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import "flag"
 
+func main() {
+	var folder string
+	flag.StringVar(&folder, "add", "", "novo diretório para análise")
+	flag.Parse()
+	print(folder)
 }
